da73e705712eb41dc717acce8e706ec63c4cf5e0
Public Dude <schiozer@yahoo.com.br>
2020-02-29 22:14:24 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,1 @@
-    // Your web app's Firebase configuration
-    var firebaseConfig = {
-        apiKey: "AIzaSyBeR4PkHmwjsMoF4wc0Bfr022rkJ952Z-I",
-        authDomain: "pocfirebase-304fb.firebaseapp.com",
-        databaseURL: "https://pocfirebase-304fb.firebaseio.com",
-        projectId: "pocfirebase-304fb",
-        storageBucket: "pocfirebase-304fb.appspot.com",
-        messagingSenderId: "974005380484",
-        appId: "1:974005380484:web:1d9ebc6374fc0f72a7e9d1",
-        measurementId: "G-055S1FTXJE"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
+
