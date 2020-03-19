2ba5a8dac4b64be90d2e922ca08556610a6f19f3
Fabio Schiozer <schiozer@yahoo.com.br>
2020-03-19 17:06:59 -0300

    move

---
+++ public/js/app_template.js
@@ -0,0 +1,18 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "bla bla bla",
+    authDomain: "YOUR firebase domain",
+    databaseURL: "Your Database URL",
+    projectId: "Your Project",
+    storageBucket: "Yours",
+    messagingSenderId: "blabla",
+    appId: "blabla",
+    measurementId: "blabla"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
