e04b6c215da808d91c1f84262b050da8d65c23ea
Fabio Schiozer <schiozer@yahoo.com.br>
2020-03-19 16:56:04 -0300

    mais

---
+++ functions/variables_template.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+
+    clientId : 'bla bla bla ',
+    clientSecret : 'bla bla bla',
+    stack : 'Sx',
+    origin : 'https://YOUR_TENANT.rest.marketingcloudapis.com/',
+    authOrigin : 'https://YOUR_TENANT.auth.marketingcloudapis.com/',
+    soapOrigin : 'https://YOUR_TENANT.soap.marketingcloudapis.com/',
+    authOptions : {
+        authVersion: 2, 
+        accountId: YOUR_MID
+    }
+};
+
